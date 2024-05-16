85d72f68a72228e808f01ddc1a86ec51741ad542
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-05-16 10:18:51 +0300

    Update MainForm.Designer.cs

--- LoginRegistrationForm/LoginRegistrationForm/MainForm.Designer.cs
+++ LoginRegistrationForm/LoginRegistrationForm/MainForm.Designer.cs
@@ -58,5 +58,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Insert;
     }
 }
