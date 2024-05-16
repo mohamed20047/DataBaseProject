053913d3e7d6b7e7ddab1da90a2c16d66cde0926
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-05-16 10:16:22 +0300

    Update MainForm.cs

--- LoginRegistrationForm/LoginRegistrationForm/MainForm.cs
+++ LoginRegistrationForm/LoginRegistrationForm/MainForm.cs
@@ -21,5 +21,23 @@ namespace LoginRegistrationForm
         {
 
         }
+        private void Insert_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\SecondYear_College\Database systems\Database Project\DataBaseProject-main v1\LoginRegistrationForm\LoginRegistrationForm\OnlineLibrary.mdf"";Integrated Security=True;Connect Timeout=30");
+        con.Open();
+        String insertData = "insert into BOOK(ISBN, TITLE, PUBLICATIONYEAR, CATEGORY) values(@ISBN, @Title, @publicationYear, @Category)";
+        using (SqlCommand command = new SqlCommand(insertData, con))
+        {
+            command.Parameters.AddWithValue("@ISBN", 5);
+            command.Parameters.AddWithValue("@Title", textBox1.Text.ToString());
+            command.Parameters.AddWithValue("@publicationYear", textBox2.Text.ToString());
+            command.Parameters.AddWithValue("@Category", textBox3.Text.ToString());
+            command.ExecuteNonQuery();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+        }
+        MessageBox.Show("Insertion completed","Insert box", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        con.Close();
+    }
+
     }
 }
