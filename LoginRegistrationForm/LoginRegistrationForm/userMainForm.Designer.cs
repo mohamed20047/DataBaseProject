f3e170bf797f3ac6e9ab663167eef0672081e861
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-05-17 23:10:22 +0300

    Update userMainForm.Designer.cs

--- LoginRegistrationForm/LoginRegistrationForm/userMainForm.Designer.cs
+++ LoginRegistrationForm/LoginRegistrationForm/userMainForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace LoginRegistrationForm
+namespace LoginRegistrationForm
 {
-    partial class userMainForm
+    partial class updateUserDetailsForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,66 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.showbooks = new System.Windows.Forms.Button();
+            this.showNewPass = new System.Windows.Forms.CheckBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LastNameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.firstNameBox = new System.Windows.Forms.TextBox();
+            this.firstname = new System.Windows.Forms.Label();
+            this.back = new System.Windows.Forms.Button();
             this.exit = new System.Windows.Forms.Button();
-            this.modify = new System.Windows.Forms.Button();
-            this.welcome = new System.Windows.Forms.Label();
+            this.update = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // showbooks
+            // showNewPass
             // 
-            this.showbooks.Location = new System.Drawing.Point(12, 144);
-            this.showbooks.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.showbooks.Name = "showbooks";
-            this.showbooks.Size = new System.Drawing.Size(299, 64);
-            this.showbooks.TabIndex = 10;
-            this.showbooks.Text = "Show Tabls";
-            this.showbooks.UseVisualStyleBackColor = true;
+            this.showNewPass.AutoSize = true;
+            this.showNewPass.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.showNewPass.ForeColor = System.Drawing.Color.Gray;
+            this.showNewPass.Location = new System.Drawing.Point(610, 157);
+            this.showNewPass.Name = "showNewPass";
+            this.showNewPass.Size = new System.Drawing.Size(125, 20);
+            this.showNewPass.TabIndex = 48;
+            this.showNewPass.Text = "Show Password";
+            this.showNewPass.UseVisualStyleBackColor = true;
+            this.showNewPass.CheckedChanged += new System.EventHandler(this.showNewPass_CheckedChanged);
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(452, 155);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.PasswordChar = '*';
+            this.PasswordBox.Size = new System.Drawing.Size(134, 22);
+            this.PasswordBox.TabIndex = 47;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(364, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Password:";
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(152, 158);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(134, 22);
+            this.emailBox.TabIndex = 45;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "Email:";
+            // 
+            // LastNameBox
+            // 
+            this.LastNameBox.Location = new System.Drawing.Point(452, 91);
+            this.LastNameBox.Name = "LastNameBox";
+            this.LastNameBox.Size = new System.Drawing.Size(130, 22);
+            this.LastNameBox.TabIndex = 43;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(350, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "Last Name:";
+            // 
+            // firstNameBox
+            // 
+            this.firstNameBox.Location = new System.Drawing.Point(152, 94);
+            this.firstNameBox.Name = "firstNameBox";
+            this.firstNameBox.Size = new System.Drawing.Size(134, 22);
+            this.firstNameBox.TabIndex = 41;
+            // 
+            // firstname
+            // 
+            this.firstname.AutoSize = true;
+            this.firstname.Location = new System.Drawing.Point(65, 97);
+            this.firstname.Name = "firstname";
+            this.firstname.Size = new System.Drawing.Size(75, 16);
+            this.firstname.TabIndex = 40;
+            this.firstname.Text = "First Name:";
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(298, 336);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(75, 23);
+            this.back.TabIndex = 39;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
             // 
             // exit
             // 
-            this.exit.Location = new System.Drawing.Point(12, 212);
-            this.exit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exit.Location = new System.Drawing.Point(475, 336);
             this.exit.Name = "exit";
-            this.exit.Size = new System.Drawing.Size(299, 64);
-            this.exit.TabIndex = 9;
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 38;
             this.exit.Text = "Exit";
             this.exit.UseVisualStyleBackColor = true;
-            this.exit.Click += new System.EventHandler(this.exit_Click_1);
+            this.exit.Click += new System.EventHandler(this.exit_Click);
             // 
-            // modify
+            // update
             // 
-            this.modify.Location = new System.Drawing.Point(12, 67);
-            this.modify.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.modify.Name = "modify";
-            this.modify.Size = new System.Drawing.Size(299, 73);
-            this.modify.TabIndex = 8;
-            this.modify.Text = "Modify";
-            this.modify.UseVisualStyleBackColor = true;
-            this.modify.Click += new System.EventHandler(this.modify_Click);
+            this.update.Location = new System.Drawing.Point(112, 336);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(75, 23);
+            this.update.TabIndex = 37;
+            this.update.Text = "Update";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click);
             // 
-            // welcome
-            // 
-            this.welcome.AutoSize = true;
-            this.welcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.welcome.Location = new System.Drawing.Point(104, 10);
-            this.welcome.Name = "welcome";
-            this.welcome.Size = new System.Drawing.Size(116, 25);
-            this.welcome.TabIndex = 6;
-            this.welcome.Text = "Welcome!!";
-            // 
-            // userMainForm
+            // updateUserDetailsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(323, 326);
-            this.Controls.Add(this.showbooks);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showNewPass);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LastNameBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.firstNameBox);
+            this.Controls.Add(this.firstname);
+            this.Controls.Add(this.back);
             this.Controls.Add(this.exit);
-            this.Controls.Add(this.modify);
-            this.Controls.Add(this.welcome);
-            this.Name = "userMainForm";
-            this.Text = "userMainForm";
-            this.Load += new System.EventHandler(this.userMainForm_Load);
+            this.Controls.Add(this.update);
+            this.Name = "updateUserDetailsForm";
+            this.Text = "updateUserDetailsForm";
+            this.Load += new System.EventHandler(this.updateUserDetailsForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,9 +177,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Button showbooks;
+        private System.Windows.Forms.CheckBox showNewPass;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox LastNameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox firstNameBox;
+        private System.Windows.Forms.Label firstname;
+        private System.Windows.Forms.Button back;
         private System.Windows.Forms.Button exit;
-        private System.Windows.Forms.Button modify;
-        private System.Windows.Forms.Label welcome;
+        private System.Windows.Forms.Button update;
     }
 }
