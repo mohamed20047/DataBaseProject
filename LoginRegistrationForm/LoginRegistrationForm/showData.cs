ea1e26f35b4d8000e8e8b6f017b8ad15eae5e247
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-05-17 23:08:47 +0300

    Update showData.cs

--- LoginRegistrationForm/LoginRegistrationForm/showData.cs
+++ LoginRegistrationForm/LoginRegistrationForm/showData.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,15 +11,18 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Contexts;
 
 namespace LoginRegistrationForm
 {
     public partial class showData : Form
     {
-        
-        public showData()
+        int adminID = 0;
+
+        public showData(int id)
         {
             InitializeComponent();
+            adminID = id;
             tableName.Items.Clear();
             tableName.Items.Add("ADMIN");
             tableName.Items.Add("AUTHOR");
@@ -44,9 +47,9 @@ namespace LoginRegistrationForm
         {
             DataTable dataTable = new DataTable();
             string replace = @"bin\Debug";
-            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="+Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace(replace, "OnlineLibrary.mdf") +";Integrated Security=True;Connect Timeout=30");
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace(replace, "onlineLibrary.mdf") + ";Integrated Security=True;Connect Timeout=30");
             con.Open();
-            if(tableName.Text == "BOOK, AUTHOR")
+            if (tableName.Text == "BOOK, AUTHOR")
             {
                 string bookNamesQuery1 = "select AUTHOR.NAME as Author_name  ,BOOK.TITLE as Book_Title , BOOK.CATEGORY as Book_Category from AUTHOR , BOOK, WRITE_BY  where AUTHOR.AUTHORID = WRITE_BY.AUTHORID and BOOK.ISBN = WRITE_BY.ISBN ";
                 SqlCommand cmd = new SqlCommand(bookNamesQuery1, con);
@@ -66,11 +69,39 @@ namespace LoginRegistrationForm
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Main main = new Main();
-            main.Show();
-            this.Hide();
+            string replace = @"bin\Debug";
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace(replace, "onlineLibrary.mdf") + ";Integrated Security=True;Connect Timeout=30");
+            con.Open();
+            String selectData = "SELECT UserType FROM UserDetails WHERE userid = " + adminID;
+            using (SqlCommand cmd = new SqlCommand(selectData, con))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read(); // Move to the first retrieved row
+                        string userType = reader.GetString(0); // Get user type from the second column
+
+                        if (userType == "Admin")
+                        {
+                            Main mForm = new Main(adminID); // Pass user ID to constructor
+                            mForm.Show();
+                        }
+                        else
+                        {
+                            userMainForm uForm = new userMainForm(adminID); // Pass user ID to constructor
+                            uForm.Show();
+                        }
+                        this.Hide();
+                    }
+
+                }
+            }
         }
 
-        
+            private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+            {
+
+            }
+        }
     }
-}
